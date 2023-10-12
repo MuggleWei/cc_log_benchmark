e0977f2878776981ef13e9407e407ef03348ad96
Muggle Wei <mugglewei@gmail.com>
2023-10-12 21:44:15 +0800

    add easyloggingpp

--- README_cn.md
+++ README_cn.md
@@ -1,5 +1,6 @@
 # 概述
-此库对几个 C/C++ 日志库做 benchmark, 被测的库包括  
+此库对几个 C/C++ 日志库做 benchmark, 被测的库如下所示(按字母顺序排序)  
+* [easyloggingpp](https://github.com/abumq/easyloggingpp): C++ logging library. It is extremely powerful, extendable, light-weight, fast performing, thread and type safe and consists of many built-in features. It provides ability to write logs in your own customized format. It also provide support for logging your classes, third-party libraries, STL and third-party containers etc.
 * [glog](https://github.com/google/glog): C++ implementation of the Google logging module
 * [haclog](https://github.com/MuggleWei/haclog): Haclog(Happy Aync C log) is an extremely fast plain C logging library
 * [nanolog](https://github.com/PlatformLab/NanoLog): Nanolog is an extremely performant nanosecond scale logging system for C++ that exposes a simple printf-like API. 
---
+++ gbenchmark/cpp/easyloggingpp/gbenchmark_easyloggingpp.cpp
@@ -0,0 +1,60 @@
+#include "benchmark/benchmark.h"
+#include "easylogging++.h"
+#include "gbenchmark/log_gbenchmark.h"
+#include "log_msg/log_msg.h"
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+std::once_flag init_flag;
+
+INITIALIZE_EASYLOGGINGPP
+
+#define LOG_FUNC(num)                                             \
+	void log_func##num(LogMsg &msg)                               \
+	{                                                             \
+		LOG(INFO) << "u64: " << msg.u64 << ", i64: " << msg.i64   \
+				  << ", u32: " << msg.u32 << ", i32: " << msg.i32 \
+				  << ", s: " << msg.s;                            \
+	}
+
+EXPAND_FUNCS
+
+class HaclogFixture : public benchmark::Fixture {
+public:
+	HaclogFixture()
+	{
+		if (!LoadLogMsg(log_msgs)) {
+			fprintf(stderr,
+					"failed load random data, Run gen_random_data first");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	void SetUp(const benchmark::State &)
+	{
+		std::call_once(init_flag, []() {
+			el::Loggers::reconfigureAllLoggers(
+				el::ConfigurationType::Format,
+				"%level|%datetime|%fbase:%line|%func|%thread - %msg");
+			el::Loggers::reconfigureAllLoggers(el::ConfigurationType::Filename,
+											   "logs/easyloggingpp.log");
+			el::Loggers::reconfigureAllLoggers(el::ConfigurationType::ToFile,
+											   "true");
+			el::Loggers::reconfigureAllLoggers(
+				el::ConfigurationType::ToStandardOutput, "false");
+			// el::Loggers::reconfigureAllLoggers(
+			//     el::ConfigurationType::MaxLogFileSize, "1073741824");
+		});
+	}
+
+	void TearDown(const benchmark::State &)
+	{
+	}
+
+public:
+	std::vector<LogMsg> log_msgs;
+};
+
+RUN_GBENCHMARK(HaclogFixture, write)
