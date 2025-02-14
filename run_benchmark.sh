ec771adce427ce8be2acf5a32cc390abc57f8250
Muggle Wei <mugglewei@gmail.com>
2025-02-14 17:02:18 +0800

    update quill benchmark target and update haclog version

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -29,8 +29,8 @@ run_benchmark glog
 run_benchmark haclog
 run_benchmark loguru
 run_benchmark nanolog
-run_benchmark quill_bounded
-run_benchmark quill_unbounded
+run_benchmark quill_bounded_blocking
+run_benchmark quill_bounded_dropping
 run_benchmark reckless
 run_benchmark spdlog_async
 run_benchmark spdlog_sync
